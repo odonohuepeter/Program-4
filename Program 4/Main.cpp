bead8565e2d678c483f3247aea1760853f53b2dc
kappz <addcolor117@gmail.com>
2017-08-17 19:21:44 -0400

    Final Version

--- Program 4/Main.cpp
+++ Program 4/Main.cpp
@@ -1,8 +1,14 @@
 /*
 Author: Peter O'Donohue
 Creation Date: 08/10/17
-Modification Date: 08/11/17
-Description: FILL IN LATER
+Modification Date: 08/17/17
+Description: The classes below allow the user to create two different types of objects, an object that contains an
+array based implementation of the stack ADT, as well as an object that contains a link list based implementation of 
+the stack ADT. Both objects contain the same methods, but each object implements these methods differently. To use,
+create an object of type 'LeakyArrayStack' or 'LeakyLinkedStack' and specify the type of data you're working with. 
+To push an item onto the stack, call the method 'push'. To remove an item from the top of the stack, call the method
+'pop'. To check if a function is empty or full, call 'isEmpty'. And finally, to view the item on top of the stack, 
+call the method 'peek'. 
 */
 
 #include <iostream>
@@ -64,14 +70,22 @@ private:
 template<class ItemType>
 LeakyArrayStack<ItemType>::LeakyArrayStack()
 {
+	/*
+	PRE: An instance of type 'LeakyArrayStack has been declared, with the data type specified by the client and the instance
+	passing through the default argument.
+    POST: An instance of type 'LeakyArrayStack' has been initialized with top = -1.
+	*/
 	top = -1;
 }
 
 template <class ItemType>
 bool LeakyArrayStack<ItemType>::isEmpty() const
 {
-
-	if (top < -1)
+	/*
+	PRE: An instance of type 'LeakyArrayStack' has been initialized and has called this function.
+	POST: The function returns true if the stack is empty, and false otherwise. 
+	*/
+	if (top < 0)
 		return true;
 	else
 		return false;
@@ -79,6 +93,12 @@ bool LeakyArrayStack<ItemType>::isEmpty() const
 template <class ItemType>
 bool LeakyArrayStack<ItemType>::push(const ItemType& newEntry)
 {
+	/*
+	PRE: An instance of type 'LeakyArrayStack' has been initialized, called this function, and is passing through
+	data of type 'ItemType'.
+	POST: The function inserts an item on top of the stack, and returns true if the item was successfully placed on 
+	top of the stack, and returns false if otherwise.
+	*/
 	bool result = false;
 	if (top < MAX_SIZE - 1)
 	{
@@ -88,9 +108,9 @@ bool LeakyArrayStack<ItemType>::push(const ItemType& newEntry)
 	}
 	else if (top == MAX_SIZE - 1)  // if stack is full
 	{
-		for (int i = 0; i < (MAX_SIZE - 1) ; ++i)  // remove oldest item from list
+		for (int i = 0; i < (MAX_SIZE - 1) ; ++i)  // remove oldest item from stack
 		{
-			items[i] = items[i + 1];
+			items[i] = items[i + 1];  // push new item on top of stack
 			result = true;
 		}
 		items[top] = newEntry;
@@ -100,6 +120,10 @@ bool LeakyArrayStack<ItemType>::push(const ItemType& newEntry)
 template <class ItemType>
 bool LeakyArrayStack<ItemType>::pop()
 {
+	/*
+	PRE: An instance of type 'LeakyArrayStack' has been initialized, and has called this function.
+	POST: The function returns true if the stack is empty, and false otherwise.
+	*/
 	bool result = false;
 	if (!isEmpty())
 	{
@@ -112,6 +136,10 @@ bool LeakyArrayStack<ItemType>::pop()
 template <class ItemType>
 ItemType LeakyArrayStack<ItemType>::peek() const
 {
+	/*
+	PRE: An instance of type 'LeakyArrayStack' has been initialized and has called this function.
+	POST: The function returns the top item to the client, and otherwise returns false.
+	*/
 	bool result = false;
 	if (!isEmpty())
 	{
@@ -139,6 +167,10 @@ public:
 template <class ItemType>
 node<ItemType>::node()
 {
+	/*
+	PRE: An instance of type 'node' has been declared with the default argument.
+	POST: The instance has been initialized with next = nullptr
+	*/
 	next = nullptr;
 }
 
@@ -146,6 +178,10 @@ node<ItemType>::node()
 template <class ItemType>
 node<ItemType>::node(const ItemType& data)
 {
+	/*
+	PRE: An instance of type 'node' has been declared with the client passing through a non default argument.
+	POST: The instance has been initialized with next = nullptr and item = data.
+	*/
 	item = data;
 	next = nullptr;
 }
@@ -175,6 +211,10 @@ private:
 template <class ItemType>
 LeakyLinkedStack<ItemType>::LeakyLinkedStack()
 {
+	/*
+	PRE: An instance of type 'LeakyLinkedStack' has been declared with the default argument.
+	POST: The instance has been initialized with next = nullptr, size = 0, and count = 0.
+	*/
 	top = nullptr;
 	size = 0;
 	count = 0;
@@ -183,14 +223,25 @@ LeakyLinkedStack<ItemType>::LeakyLinkedStack()
 template <class ItemType>
 LeakyLinkedStack<ItemType>::LeakyLinkedStack(const int& SIZE)
 {
-	size = SIZE;
-	top = nullptr;
-	count = 0;
+	/*
+	PRE: An instance of type 'LeakyLinkedStack' has been declared with a non default argument.
+	POST: The instance has been initialized with next = nullptr, size = SIZE, and count = 0.
+	*/
+	if (SIZE >= 0)
+	{
+		size = SIZE;
+		top = nullptr;
+		count = 0;
+	}
 }
 
 template <class ItemType>
 LeakyLinkedStack<ItemType>::LeakyLinkedStack(const LeakyLinkedStack<ItemType>& original)
 {
+	/*
+	PRE: An operation requiring a copy of an instance of type 'LeakyLinkedStack' has been initiated.
+	POST: The instance has been deep copied.
+	*/
 	size = original.size;
 	node<ItemType> *copyNode = nullptr;
 	node<ItemType> *oldPtr = original.top;
@@ -211,29 +262,30 @@ LeakyLinkedStack<ItemType>::LeakyLinkedStack(const LeakyLinkedStack<ItemType>& o
 		copyNode = copyNode->next;
 		oldPtr = oldPtr->next;
 	}
-	cout << "Copy constructor called." << endl;
-	return *this;
 }
 
 template <class ItemType>
 LeakyLinkedStack<ItemType>::~LeakyLinkedStack()
 {
-
-
+	/*
+	PRE: An instance of type 'LeakyLinkedStack' has left scope, and or a copy constructor or assignment overload has 
+	been performed.
+	POST: The instance has been deallocated from memory.
+	*/
 	while (!isEmpty())
 	{
-		cout << "test" << endl;
 		pop();
 	}
-
-	cout << "Destructor called." << endl;
-
 }
 
 
 template <class ItemType>
 LeakyLinkedStack<ItemType>LeakyLinkedStack<ItemType>::operator=(const LeakyLinkedStack<ItemType>& rhs)
 {
+	/*
+	PRE: An assignment operation on an instance of type 'LeakyLinkedStack' has been initialized.
+	POST: The instance on the right hand side has been copied the the instance on the left hand side.
+	*/
 	this->size = rhs->size;
 	node<ItemType>lhsWalker = nullptr;
 	node<ItemType> rhsWalker = rhs->top;
@@ -254,7 +306,10 @@ LeakyLinkedStack<ItemType>LeakyLinkedStack<ItemType>::operator=(const LeakyLinke
 template <class ItemType>
 bool LeakyLinkedStack<ItemType>::isEmpty() const
 {
-
+	/*
+	PRE: An instance of type 'LeakyLinkedStack' has been initialized and has called this function.
+	POST: The function returns true if the stack is empty, and false otherwise.
+	*/
 	if (top == nullptr)
 	{
 		return true;
@@ -266,6 +321,12 @@ bool LeakyLinkedStack<ItemType>::isEmpty() const
 template <class ItemType>
 bool LeakyLinkedStack<ItemType>::push(const ItemType& newEntry)
 {
+	/*
+	PRE: An instance of type 'LeakyLinkedStack' has been initialized and has called this function, passing through
+	a data argument of type 'ItemType'
+	POST: the data in the argument has been pushed onto the stack, and the function returns true if successfull, and false
+	if unsuccessfull.
+	*/
 	node<ItemType> *tempTop = nullptr;
 	if (top == nullptr)
 	{
@@ -274,37 +335,36 @@ bool LeakyLinkedStack<ItemType>::push(const ItemType& newEntry)
 		tempTop->next = nullptr;
 		top = tempTop;
 		tempTop = nullptr;
-		++count;
+		if (count < size);
+			++count;
 		return true;
 	}
 
-	else if (count == size)
+	else if (count == size) // if stack is full
 	{
-
 		node<ItemType> *walker = nullptr;
 		walker = top;
 		while (walker->next != nullptr)
 		{
 			walker = walker->next;
 		}
-		delete walker;
+		delete walker;  // remove oldest item from stack
 		walker = nullptr;
-		tempTop = new node<ItemType>;
+		tempTop = new node<ItemType>;  // place new item on top of stack
 		tempTop->item = newEntry;
 		tempTop->next = top;
 		top = tempTop;
 		tempTop = nullptr;
 		return true;
-
 	}
-
 	else
 	{
 		tempTop = new node<ItemType>;
 		tempTop->item = newEntry;
 		tempTop->next = top;
 		top = tempTop;
-		++count;
+		if (count < size)
+			++count;
 		return true;
 	}
 	return false;
@@ -313,6 +373,10 @@ bool LeakyLinkedStack<ItemType>::push(const ItemType& newEntry)
 template <class ItemType>
 ItemType LeakyLinkedStack<ItemType>::peek() const
 {
+	/*
+	PRE: An instance of type 'LeakyLinkedStack' has been initialized and has called this function.
+	POST: The function returns the item on top of the stack to the client if the stack is not empty.
+	*/
 	if (!isEmpty())
 	{
 		ItemType result = top->item;
@@ -323,6 +387,11 @@ ItemType LeakyLinkedStack<ItemType>::peek() const
 template <class ItemType>
 bool LeakyLinkedStack<ItemType>::pop()
 {
+	/*
+	PRE: An instance of type 'LeakyLinkedStack' has been initialized and has called this function.
+	POST: The function removes the top item from the stack if not empty, and returns true is successfull, and false if
+	unsuccessfull.
+	*/
 	bool result = false;
 	if (!isEmpty())
 	{
@@ -339,10 +408,26 @@ bool LeakyLinkedStack<ItemType>::pop()
 	return result;
 }
 
+// unit test protoypes
+template <class ItemType>
+bool testPushAndPop(LeakyArrayStack<ItemType> a, LeakyLinkedStack<ItemType> b);  // tests push and pop
+template <class ItemType>
+bool testIsEmpty(LeakyArrayStack<ItemType> a, LeakyLinkedStack<ItemType> b);  // tests isEmpty
+template <class ItemType>
+bool testPeek(LeakyArrayStack<ItemType> a, LeakyLinkedStack<ItemType> b);  // tests peek
+
 int main()
 {
-	LeakyArrayStack<int> arrayStack;
-	LeakyLinkedStack<int> linkStack(5);
+	LeakyArrayStack<int> c;  // for unit tests
+	LeakyLinkedStack<int> d(5);  // for unit tests
+	LeakyArrayStack<int> e;  // for unit tests
+	LeakyLinkedStack<int> f(5);  // for unit tests
+	LeakyArrayStack<int> g;  // for unit tests
+	LeakyLinkedStack<int> h(5);  // for unit tests
+	LeakyArrayStack<int> arrayStack;  // test sample input and output
+	LeakyLinkedStack<int> linkStack(5);  // test sample input and output
+	
+	// test sample input and output
 	arrayStack.push(2);
 	arrayStack.push(3);
 	arrayStack.push(4);
@@ -386,6 +471,106 @@ int main()
 	else
 		cout << "Leaky link stack is not empty." << endl;
 
+	//unit tests
+	if (!testPushAndPop(e, f))
+		cout << "testPushAndPop failed unit test." << endl;
+	else
+		cout << "testPushAndPop passed unit test." << endl;
+	if (!testIsEmpty(e, f))
+		cout << "testIsEmpty failed unit test." << endl;
+	else
+		cout << "testIsEmpty passed unit test." << endl;
+	if (!testPeek(g, h))
+		cout << "testIsFull failed unit test." << endl;
+	else
+		cout << "testIsFull passed unit test." << endl;
+
 	system("pause");
 	return 0;
+}
+
+// unit tests implementations
+template <class ItemType>
+bool testPushAndPop(LeakyArrayStack<ItemType> a, LeakyLinkedStack<ItemType> b)
+{
+	bool result = true;
+	for (int i = 0; i <= 5; ++i)  // fill stack and push when stack is full
+	{
+		int foo;
+		a.push(i);
+		foo = a.peek();
+		if (foo != i)
+			result = false;
+		b.push(i);
+		foo = b.peek();
+		if (foo != i)
+			result = false;
+	}
+	for (int i = 0; i < 5; ++i)  // empty stack
+	{
+		a.pop();
+		b.pop();
+	}
+	
+	if (!a.isEmpty())
+		result = false;
+	if (!b.isEmpty())
+	    result = false;
+	return result;
+}
+
+template <class ItemType>
+bool testIsEmpty(LeakyArrayStack<ItemType> a, LeakyLinkedStack<ItemType> b)
+{
+	bool result = true;
+
+	// verify initialized stacks are empty.
+	if (!a.isEmpty())
+		result = false;
+	if (!b.isEmpty())
+		result = false;
+
+	// test isEmpty after one push
+	a.push(5);
+	b.push(5);
+	if (a.isEmpty())
+		result = false;
+	if (b.isEmpty())
+		result = false;
+
+	// test isEmpty after each stack has been empty
+	a.pop();
+	b.pop();
+	if (!a.isEmpty())
+		result = false;
+	if (!b.isEmpty())
+		result = false;
+
+	return result;
+}
+
+template <class ItemType>
+bool testPeek(LeakyArrayStack<ItemType> a, LeakyLinkedStack<ItemType> b)
+{
+	bool result = true;
+	if (a.peek())
+		result = false;
+	if (a.peek())
+		result = false;
+
+	for (int i = 0; i <= 5; ++i)  // fill stack and test peek for each item
+	{
+		int foo;
+		a.push(i);
+		foo = a.peek();
+		if (foo != i)
+			result = false;
+		b.push(i);
+		foo = b.peek();
+		if (foo != i)
+			result = false;
+	}
+	return result;
+
+	
 }
